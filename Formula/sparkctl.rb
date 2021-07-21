c799acf93dd5144bbaaf5ce773ecdd686f6320d1
Kyle McGovern <kmcgovern@apixio.com>
2021-07-21 16:36:39 -0700

    sparkctl 1.2.2: update url, test, and go version

--- Formula/sparkctl.rb
+++ Formula/sparkctl.rb
@@ -1,29 +1,26 @@
 class Sparkctl < Formula
   desc "CLI for interacting with spark on k8s"
   homepage "https://github.com/GoogleCloudPlatform/spark-on-k8s-operator"
-  url "https://github.com/GoogleCloudPlatform/spark-on-k8s-operator.git",
-      tag:      "v1beta2-1.2.2-3.0.0",
-      revision: "2e8b733f5ad029ceb8a949b153a53c434b8fcfe1"
+  url "https://github.com/GoogleCloudPlatform/spark-on-k8s-operator/archive/refs/tags/v1beta2-1.2.2-3.0.0.tar.gz"
+  version "1.2.2"
+  sha256 "72fd2e1967ea20c82718c011558fa863c06cdd29cfae65ffc02177da2446a2a9"
   # the versioning scheme is apiversion-operatorVersion-sparkVersion.
   # Homebrew will automatically detect the spark version by default
   # instead pull the operator version which more closely matches to sparkctl
-  version "1.2.2"
   license "Apache-2.0"
   head "https://github.com/GoogleCloudPlatform/spark-on-k8s-operator.git"
-  depends_on "go@1.15" => :build
+  depends_on "go" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
     ENV["CGO_ENABLED"] = "0"
-    dir = buildpath/"src/github.com/GoogleCloudPlatform/spark-on-k8s-operator"
-    dir.install (buildpath/"").children
-    cd dir/"sparkctl" do
+    cd buildpath/"sparkctl" do
       system "go", "build", *std_go_args
     end
   end
 
   test do
-    system "test", "-f", "#{bin}/sparkctl"
     system "#{bin}/sparkctl", "--help"
+    assert_match(/failed to get SparkApplication client: unable to load in-cluster configuration,.*/,
+                 shell_output("#{bin}/sparkctl list 2>&1"))
   end
 end
